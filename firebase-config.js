f81f536870f810c9c65e69f1eebe73faecb88ca8
macheng20040121-bit <macheng20040121@gmail.com>
2026-07-03 23:32:35 +0800

    Deploy firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Fill this after creating a Firebase web app and Firestore database.
+// When left as-is, the planner stays fully usable in offline/local mode.
+window.TRIP_FIREBASE_CONFIG = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
